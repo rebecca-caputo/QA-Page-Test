a13873884c9dc0c69cefcfce9a9b594f0ce1f8fd
Rebecca Caputo <beccalynncaputo@gmail.com>
2025-08-06 21:23:50 -0400

    Add files via upload

---
+++ firstTest.spec.ts
@@ -0,0 +1,37 @@
+import {test} from '@playwright/test'
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+})
+
+test.describe('suite1', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Charts').first().click();
+    })
+
+    test('the first test', async ({page}) => {
+        await page.getByText('Forms').click();
+        await page.getByText('Form Layouts').click();
+    })
+    
+    test('navigate to datepicker page', async ({page}) => {
+        await page.getByText('Forms').click();
+        await page.getByText('Datepicker').click();
+    })
+})
+
+test.describe('suite2', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Forms').click();
+    })
+
+    test('the second test', async ({page}) => {
+        await page.getByText('Forms').click();
+        await page.getByText('Form Layouts').click();
+    })
+    
+    test('navigate to datepicker page again', async ({page}) => {
+        await page.getByText('Forms').click();
+        await page.getByText('Datepicker').click();
+    })
+})
